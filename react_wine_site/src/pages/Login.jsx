b99e1378be4e9e2d3188403f9f6411148956fd3d
iammmila <ibramila2002@gmail.com>
2023-01-04 16:15:17 +0400

    loginForm

--- react_wine_site/src/pages/Login.jsx
+++ react_wine_site/src/pages/Login.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 import "./../assets/_login.scss"
 function Login() {
   return (
-    <>
+    <div className='logRegPage'>
       <div className='register'>
         <h1> Register</h1>
         <form action="">
-          <input type="text" placeholder='Name' />
-          <input type="text" placeholder='Surname' />
-          <input type="text" placeholder='Age' />
-          <input type="text" placeholder='Age' />
-          <input type="text" placeholder='Email' />
-          <input type="text" placeholder='password' />
-          <input type="text" />
-          <input type="text" />
-
+          <input type="text" placeholder='Name' required />
+          <input type="text" placeholder='Surname' required />
+          <input type="number" placeholder='Age' required />
+          <input type="email" placeholder='Email' required />
+          <input type="password" placeholder='Password' required />
+          <input type="password" placeholder='Password again' required/>
+          <input type="number" placeholder='Phone Number' required />
+          <button type="submit" id="login-button">Sign Up</button>
         </form>
       </div>
 
-    </>
+    </div>
   )
 }
-//!name, surname, age, email, password, repeat password, phone ve gender
+
 export default Login
